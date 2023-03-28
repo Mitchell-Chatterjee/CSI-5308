5371269b0498f98cac01ed7c9fd1a12685fdf8f4
Mitchell-Chatterjee <mitchell.chatterjee@gmail.com>
2023-03-28 11:04:06 -0400

    Set up the ring

--- Project-Python/Ring.py
+++ Project-Python/Ring.py
@@ -43,3 +43,37 @@ class Node:
             return [self._value, self._left.value]
         return [self._value, self._right.value]
 
+
+class Ring:
+    def __init__(self, nodes: [Node], direction: Direction):
+        self._nodes = nodes
+        self._direction = direction
+
+        self.create_ring()
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        self._direction = direction
+
+    def create_ring(self):
+        """
+        This function will string up the nodes in order to form a ring. From left to right.
+        :return: Returns the same list of nodes, that can then be used for running algorithms.
+        """
+        # Connect each node to the one to their right and vice versa
+        for i in range(0, len(self._nodes)-1):
+            self._nodes[i].right = self._nodes[i+1]
+            self._nodes[i+1].left = self._nodes[i]
+
+        # String up the last node to the first node and vice versa
+        self._nodes[-1].right = self._nodes[0]
+        self._nodes[0].left = self._nodes[-1]
+
--- Project-Python/main.py
+++ Project-Python/main.py
@@ -1,19 +1,23 @@
 import igraph as ig
-from Ring import Node, Direction
+from Ring import Node, Direction, Ring
 
 
 def run_experiments():
     # Let's generate a couple nodes to start and make sure we can graph them properly
-    node_1 = Node(3, None, None)
+    node_1 = Node(1, None, None)
     node_2 = Node(2, None, None)
+    node_3 = Node(3, None, None)
 
-    # Now let's link up the nodes
-    node_1.left, node_1.right = node_2, node_2
-    node_2.left, node_2.right = node_1, node_1
+    # Now let's link up the nodes in a ring
+    ring = Ring([node_1, node_2, node_3], Direction.RIGHT)
 
     # Now let's ensure they give the correct edges
     print(f"Node 1 edge left: {node_1.get_edge(Direction.LEFT)}")
     print(f"Node 2 edge right: {node_2.get_edge(Direction.RIGHT)}")
+
+    # Print all edges in order
+    edges = [elem.get_edge(Direction.RIGHT) for elem in ring.nodes]
+    print(edges)
 
 
 if __name__ == '__main__':
